de103ffdac4f1f544f650cb07cd19ae78c84cb2b
Adam Stankiewicz <41164873+AdamJStankiewicz@users.noreply.github.com>
2024-05-07 00:36:35 -0400

    Mostly finished
    Emails are now implemented and can be used to search for tickets

--- server.py
+++ server.py
@@ -1,52 +1,112 @@
 from flask import *
 from flask_socketio import SocketIO, emit
+
 import uuid
+from uuid import UUID
+
 import json
 import os
 
-storage_path = 'storage/storage.json'
-test_desc = "Djo is such a fucking vibe"
+main_storage_path = 'storage/storage.json'
+email_storage_path = 'storage/emails.json'
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 class storage:
     data = {}
+    email_data = {}
 
     def store_ticket(id,new_ticket):
         if storage.data == {}:
             storage.retrieve_data()
 
         storage.data[id] = new_ticket
+
+        if new_ticket["email"] != "":
+            storage.email_data[new_ticket["email"]] = id
+
         with open("storage/storage.json", "w") as outfile:
             json.dump(storage.data,outfile)
+        
+        with open("storage/emails.json", "w") as outfile:
+            json.dump(storage.email_data,outfile)
 
 
     def retrieve_data():
-        if not os.path.isfile(storage_path):
+        if not os.path.isfile(main_storage_path):
             print("Storage file does not exist. It will be created once the first ticket has been created.")
             return
+        
         with open('storage/storage.json', 'r') as openfile:
             json_object = json.load(openfile)
+            storage.data = json_object
+
+        if not os.path.isfile(email_storage_path):
+            print("Email storage file does not exist. It will be created once the first ticket has been created.")
+            return
         
-        storage.data = json_object
+        with open('storage/emails.json', 'r') as openfile:
+            json_object = json.load(openfile)
+            storage.email_data = json_object
+
+
+    def check_for_email(email):
+        storage.retrieve_data()
+        return str(email in storage.email_data)
 
 class ticket:
-    def create_ticket(desc):
-        ticket_data = {"desc" : desc}
+    def create_ticket(data):
+        ticket_data = data
         new_id = ticket.create_uuid()
+        res = { "id" : "",
+                "status" : "",
+                "ticket" : ""}
+
+        if data["email"] != "":
+            if data["email"] in storage.email_data:
+                res["status"] = "1"
+                res["id"] = ticket.retrive_uuid(data["email"])
+                res["ticket"] = ticket.read_ticket_by_email(data["email"])
+                return res
 
         storage.store_ticket(new_id, ticket_data)
+        res["status"] = "0"
+        res["id"] = new_id
+        res["ticket"] = ticket_data
+        
+        return res
+
+    def read_all_tickets():
+        storage.retrieve_data()
+        return storage.data
 
     def read_ticket(id):
+        storage.retrieve_data()
+
         if id in storage.data:
             return storage.data[id]
+
+        return str("Ticket with UUID: " + id + " Was not found.")
+
+    def read_ticket_by_email(email):
+        storage.retrieve_data()
+
+        if email in storage.email_data:
+            return storage.data[storage.email_data[email]]
         
-        return str("Ticket with UUID: " + id + " Was not found")
+        return str("Ticket with EMAIL: " + email + " Was not found.")
+
+    def retrive_uuid(email):
+        storage.retrieve_data()
+        if email in storage.email_data:
+            return storage.email_data[email]
+
+        return str("Email: " + email + " Was not found.")
 
     def create_uuid():
         id = str(uuid.uuid1())
         return id
-
 
 @app.route('/', methods=['GET'])
 def main():
@@ -54,19 +114,23 @@ def main():
 
 @app.route('/tickets/', methods=['GET'])
 def get_tickets():
-    storage.retrieve_data()
-    return storage.data
+    ticket.read_all_tickets()
 
-@app.route('/tickets/<string:id>/', methods=['GET'])
+@app.route('/ticket_by_id/<string:id>/', methods=['GET'])
 def get_ticket(id):
-    storage.retrieve_data()
-    return storage.data[id]
+    return ticket.read_ticket(id)
 
-@app.route('/upload', methods=['POST'])
+@app.route('/ticket_by_email/<string:email>/', methods=['GET'])
+def get_ticket_by_email(email):
+    return ticket.read_ticket_by_email(email)
+
+@app.route('/check_for_email/<string:email>/', methods=['GET'])
+def check_for_email(email):
+    return storage.check_for_email(email)
+
+
+@app.route('/upload', methods=['GET','POST'])
 def upload_ticket():
-    desc = request.json["desc"]
-    ticket.create_ticket(desc)
-
-    return "Ticket uploaded"
+    return ticket.create_ticket(request.json)
 
 socketio.run(app,host="0.0.0.0",port=1477, allow_unsafe_werkzeug=True, debug=True)
--- upload_ticket.py
+++ upload_ticket.py
@@ -2,6 +2,6 @@ import json
 import requests
 
 api_url = 'http://127.0.0.1:1477/upload'
-create_row_data = {'desc' : "Test post!!!"}
+create_row_data = {'desc' : "This is a test description that was set in a python file.", "email" : "test@gmail.com"}
 r = requests.post(url=api_url, json=create_row_data)
 print(r.status_code,r.reason,r.text)
